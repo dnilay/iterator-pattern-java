dff2b637506b0d1c80cabd619bab0c31a1cd99c9
Nilay <mailnilaydas@gmail.com>
2023-02-22 12:18:17 +0530

    2nd commit

---
+++ src/main/java/org/example/Circle.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Circle implements Shape{
+    @Override
+    public void draw() {
+        // /Print statement to execute when
+        // draw() method of this class is called
+        // later on in the main() method
+        System.out.println("Shape: Circle");
+    }
+}
---
+++ src/main/java/org/example/Shape.java
@@ -0,0 +1,8 @@
+package org.example;
+
+// Interface named Shape
+public interface Shape {
+
+	// Method inside interface
+	void draw();
+}
